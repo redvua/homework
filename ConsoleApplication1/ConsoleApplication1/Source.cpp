8e68af5506c11cae61e42a0406c5045318ba5ce7
Ed Babich <ed.babich@gmail.com>
2017-03-24 15:09:31 +0200

    add app1

---
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,19 @@
+/* 1. Написати функцію, яка отримує рядок і повертає довжину рядка. Без використання функції strlen() */
+#include <iostream>
+using namespace std;
+
+size_t StringLength(char* str) {
+	size_t out = 0;
+	while (str[out] != '\0') ++out;
+	return out;
+}
+void main() {
+	const size_t MaxStringSize = 255;
+	char* string = new char[MaxStringSize];
+	do {
+		cout << "Enter the string (or nothing to exit): ";
+		cin.getline(string, MaxStringSize);
+		if (*string == '\0') continue;
+		cout << "String Length = " << StringLength(string) << endl;
+	} while (*string != '\0');
+}
