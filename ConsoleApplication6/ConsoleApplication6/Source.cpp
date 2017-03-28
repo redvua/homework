eb586fdf5dc6776e29bbed9b840d2a75cdbf0571
Ed Babich <ed.babich@gmail.com>
2017-03-28 14:14:27 +0300

    add functions from Fraction -- inc, dec, mul, div, reduction, show

--- ConsoleApplication6/ConsoleApplication6/Source.cpp
+++ ConsoleApplication6/ConsoleApplication6/Source.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+const size_t MaxStringSize = 255;
+
 struct Fraction
 {
-	enum Action { Sum, Sub, Mul, Div, Actions = Div };
+	// enum Action { Sum, Sub, Mul, Div, Actions = Div };
 	int numerator;
 	int denominator;
 
@@ -14,6 +16,48 @@ struct Fraction
 	Fraction () :Fraction(0,1)
 	{
 
+	}
+	Fraction* Inc(Fraction item)
+	{
+		this->numerator = numerator*item.denominator + item.numerator*denominator;
+		this->denominator *= item.denominator;
+		return this;
+	}
+	Fraction* Dec(Fraction item)
+	{
+		this->numerator = numerator*item.denominator - item.numerator*denominator;
+		this->denominator *= item.denominator;
+		return this;
+	}
+	Fraction* Mul(Fraction item)
+	{
+		this->numerator = numerator * item.numerator;
+		this->denominator *= item.denominator;
+		return this;
+	}
+	Fraction* Div(Fraction item)
+	{
+		this->numerator = numerator * item.denominator;
+		this->denominator *= item.numerator;
+		return this;
+	}
+	void Reduction() {	
+		int min = (numerator < denominator) ? numerator : denominator;
+		for (; min > 1; --min)
+		{
+			if (!(numerator%min || denominator%min))
+			{
+				numerator /= min;
+				denominator /= min;
+			}
+		}
+	}
+	void Show()
+	{
+		if (numerator / denominator) printf("%d ", numerator / denominator); // целая часть
+		Reduction(); // сокращение
+		if (numerator%denominator) printf("%d/%d", numerator%denominator, denominator); // дробная часть
+		cout << endl;
 	}
 };
 struct Solution
@@ -28,17 +72,6 @@ struct Solution
 		this->buff = buff;
 		this->error = false;
 		this->subject = nullptr;
-
-
-		for (size_t i = 0; buff[i] != '\0'; i++)
-		{
-			int num = 0;
-			if (isdigit(buff[i])) // проверить отрицательные -12
-			{
-				num = atoi(buff);
-			}
-		}
-
 
 		this->result = Fraction(); //
 	}
@@ -74,9 +107,14 @@ struct Parser
 };
 void main()
 {
-	const size_t MaxStringSize = 255;
-	char* string = new char[MaxStringSize] {"1/2 + 2/3"};
-	Solution wrap = Solution(string);
+	char* string = new char[MaxStringSize] {"1/2 + 2/3 ="};
+	// Solution wrap = Solution(string);
+	Fraction{ 13,169 }.Show();
+	Fraction{ 5,4 }.Inc({ 1,2 })->Mul({1,2})->Show();
+	Fraction{ 3,4 }.Mul({ 11,18 })->Show();
+	//Fraction{ 3,5 }.Dec({ 1,2 })->Show();
+	//Fraction{ 9,2 }.Div({ 1,2 })->Show();
+	cout << endl;
 
 
 }
