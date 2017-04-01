43187176fc8747d78a1be1dd39b379fff0e6bcf4
Ed Babich <ed.babich@gmail.com>
2017-04-02 00:31:47 +0300

    add processing Fraction.integer

--- ConsoleApplication6/ConsoleApplication6/Source.cpp
+++ ConsoleApplication6/ConsoleApplication6/Source.cpp
@@ -4,47 +4,58 @@ const size_t MaxStringSize = 255;
 
 struct Fraction
 {
+	int integer;
 	int numerator;
 	int denominator;
-	//Fraction* (*act)(Fraction) = this.Inc;
 
-
-	Fraction(int numerator, int denominator)
+	Fraction(int numerator = 0, int denominator = 1, int integer = 0)
 	{
 		this->numerator = numerator;
-		this->denominator = denominator;
-	}
-	Fraction () :Fraction(0,1)
-	{
-
+		this->denominator = denominator; // !=0
+		this->integer = integer;
 	}
 	Fraction* Inc(Fraction item)
 	{
+		this->integer = integer + item.integer;
 		this->numerator = numerator*item.denominator + item.numerator*denominator;
 		this->denominator *= item.denominator;
 		return this;
 	}
 	Fraction* Dec(Fraction item)
 	{
+		this->integer = integer - item.integer;
 		this->numerator = numerator*item.denominator - item.numerator*denominator;
 		this->denominator *= item.denominator;
 		return this;
 	}
 	Fraction* Mul(Fraction item)
 	{
+		Induction();
+		item.Induction();
 		this->numerator = numerator * item.numerator;
 		this->denominator *= item.denominator;
 		return this;
 	}
 	Fraction* Div(Fraction item)
 	{
+		Induction();
+		item.Induction();
 		this->numerator = numerator * item.denominator;
-		this->denominator *= item.numerator;
+		this->denominator *= item.numerator; // !=0
 		return this;
+	}
+	void Induction()
+	{
+		numerator = numerator + integer*denominator;
+		integer = 0;
 	}
 	void Reduction()
 	{
+		Induction();
 		if (numerator < 0 && denominator < 0) this->Mul({-1, -1}); // сокращение знака
+		integer = numerator / denominator;
+		if (integer) numerator = numerator%denominator;
+
 		int min = (numerator < denominator) ? numerator : denominator;
 		for (; min > 1; --min)
 		{
@@ -57,11 +68,12 @@ struct Fraction
 	}
 	void Show()
 	{
-		if (numerator / denominator) printf("%d ", numerator / denominator); // целая часть
 		Reduction(); // сокращение
-		if (numerator%denominator) printf("%d/%d", numerator%denominator, denominator); // дробная часть
-		if (!numerator) cout << "0";
-		// cout << endl;
+		if (integer) cout << integer; // целая часть
+		if (integer && numerator) cout << "_"; // разделитель
+		if (numerator) cout << numerator << "/" << denominator; // дробная часть
+		if (!integer && !numerator) cout << "0";
+		cout << endl;
 	}
 };
 struct Solution
@@ -86,9 +98,9 @@ struct Solution
 		state = Sstate;
 		while (*this->buff != '\0' && !error) Step();
 
-		if (error) cout << "Error: " << this->buff;
+		if (error) cout << "error ->" << this->buff << endl;
 		else result.Show();
-		cout << endl;
+		//cout << endl;
 	}
 	//Solution() :Solution(nullptr)
 	//{
@@ -100,17 +112,21 @@ struct Solution
 		switch (state)
 		{
 		case Sstate:
+			if (*buff == ' ') ++buff;
+			//else if (*buff == '(') ++buff, subj = Solution(buff).result;
+			else
+			{
 			subj.numerator = strtol(buff, &buff, 10); // обрабатывает цифру ноль в том числе
 			if (offset < buff) state = Astate;
 			else error = true;
-			//if (*buff == ' ') ++buff;
+			}
 			break;
 		case Astate:
 			if (*buff == ' ') ++buff;
-			else if (*buff == '+') result.Inc(subj), ++buff, subj = { 0,1 }, state = Sstate;
-			else if (*buff == '-') result.Inc(subj), ++buff, subj = { 0,-1 }, state = Sstate;
 			else if (*buff == '*') ++buff, state = Mstate;
 			else if (*buff == '/') ++buff, state = Dstate;
+			else if (*buff == '+') result.Inc(subj), ++buff, subj = { 0,1 }, state = Sstate;
+			else if (*buff == '-') result.Inc(subj), ++buff, subj = { 0,-1 }, state = Sstate;
 			else if (*buff == '=') result.Inc(subj), ++buff, subj = { 0,1 }, error = (*buff != '\0'), state = End;
 			else error = true;
 			break;
@@ -142,11 +158,11 @@ void main()
 	wrap = Solution(" 12 - 5 + 10 =");
 	wrap = Solution(" 0*2 -5 * -3/-6 + 10 =");
 	wrap = Solution(" ( - 5 + 10 =");
-	//Fraction{ 13,169 }.Show();
+	Fraction{ 13,169 }.Show();
 	//Fraction{ 5,4 }.Inc({ 1,2 })->Mul({1,2})->Show();
 	//Fraction{ 3,4 }.Mul({ 11,18 })->Show();
 	//Fraction{ 3,5 }.Dec({ 1,2 })->Show();
-	//Fraction{ 9,2 }.Div({ 1,2 })->Show();
+	Fraction{ 9,2 }.Div({ 1,2 })->Show();
 	cout << endl;
 
 }
